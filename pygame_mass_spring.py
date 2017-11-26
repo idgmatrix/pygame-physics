3826b111af1fd12cb1f23ec78ed5eefbcfb535e9
idgmatrix <idgmatrix@gmail.com>
2017-11-27 00:14:01 +0900

    mass spring system
    mass spring system
    Euler’s method

---
+++ pygame_mass_spring.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 23:22:19 2017
+
+@author: kaswan
+"""
+
+import pygame, sys, math
+
+pygame.init()
+
+FPS = 60 # frames per second setting
+fpsClock = pygame.time.Clock()
+
+# set up the window
+screen = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Mass Spring System')
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+delta_t = 0.1
+m = 1
+k = 0.5 #Spring Constant
+
+x = 0
+y = 250
+
+vx = 10
+vy = 0
+
+while True:
+    #screen.fill(BLACK)
+
+    fx = 0
+    fy = -k * (y-150)
+
+    vx = vx + (fx / m) * delta_t
+    vy = vy + (fy / m) * delta_t
+
+    x = x + vx * delta_t
+    y = y + vy * delta_t
+
+    if x > 400:
+        screen.fill(BLACK)
+        x = 0
+        
+    pygame.draw.circle(screen, WHITE, (int(x), int(y)), 3)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
+
+
