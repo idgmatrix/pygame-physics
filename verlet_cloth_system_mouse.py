8e0da49f7df5e852c87915ae2d5e04fbf8aa211d
idgmatrix <idgmatrix@gmail.com>
2017-11-30 02:22:49 +0900

    mouse control
    add mouse control

--- verlet_cloth_system_mouse.py
+++ verlet_cloth_system_mouse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 27 19:43:01 2017
+Created on Thu Nov 30 01:59:16 2017
 
 @author: kaswan
 """
@@ -17,7 +17,7 @@ fpsClock = pygame.time.Clock()
 WIDTH = 400
 HEIGHT = 300
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-pygame.display.set_caption('Verlet Simple Rigid Body System')
+pygame.display.set_caption('Verlet Simple Cloth System')
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -28,24 +28,18 @@ class Particle:
         self.m = m
         self.x = x
         self.y = y
-        self.oldx = x-10
+        self.oldx = x
         self.oldy = y
         self.newx = x
         self.newy = y
         self.ax = 0
         self.ay = 9.8
         
-        self.selected = False
         self.fixed = False
+        self.selected = False
         
     def update(self, delta_t):
         if self.fixed == False:
-            # Collision Process
-            if self.x < 0 or self.x > WIDTH:
-                self.x, self.oldx = self.oldx, self.x
-            if self.y < 0 or self.y > HEIGHT:
-                self.y, self.oldy = self.oldy, self.y
-    
             # Verlet Integration
             self.newx = 2.0 * self.x - self.oldx + self.ax * delta_t * delta_t 
             self.newy = 2.0 * self.y - self.oldy + self.ay * delta_t * delta_t 
@@ -53,20 +47,25 @@ class Particle:
             self.oldy = self.y
             self.x = self.newx
             self.y = self.newy
-        
-        if self.selected:
+            
+            # Collision Process
+            if self.x < 0 or self.x > WIDTH:
+                self.x, self.oldx = self.oldx, self.x
+            if self.y < 0 or self.y > HEIGHT:
+                self.y, self.oldy = self.oldy, self.y
+                
+        if self.selected == True:
             pos = pygame.mouse.get_pos()
             self.x = pos[0]
             self.y = pos[1]
-            
         if mouse == False:
             self.selected = False
-        
+
     def draw(self, surf, size):
         if self.selected == True:
             color = RED
         else:
-            color = WHITE
+            color = WHITE        
         pygame.draw.circle(surf, color, (int(self.x), int(self.y)), size)
         
 class Constraint:
@@ -78,12 +77,11 @@ class Constraint:
         self.restLength = math.sqrt(delta_x * delta_x + delta_y * delta_y)
         
     def update(self):
-        
         delta_x = particles[self.index1].x - particles[self.index0].x
         delta_y = particles[self.index1].y - particles[self.index0].y
         deltaLength = math.sqrt(delta_x * delta_x + delta_y * delta_y)
-        diff = (deltaLength - self.restLength)/(deltaLength + 0.001)
-        
+        diff = (deltaLength - self.restLength)/deltaLength
+
         if particles[self.index0].fixed == False:
             particles[self.index0].x += 0.5 * diff * delta_x
             particles[self.index0].y += 0.5 * diff * delta_y
@@ -97,20 +95,7 @@ class Constraint:
         x1 = particles[self.index1].x
         y1 = particles[self.index1].y
         pygame.draw.line(surf, WHITE, (int(x0), int(y0)), (int(x1), int(y1)), size)
-            
-delta_t = 0.1
-NUM_ITER = 10
-mouse = False
-
-# create particles
-particles = []
-for i in range(3):
-    x = 20.0 * math.cos(math.radians(120) * i)
-    y = 20.0 * math.sin(math.radians(120) * i)
-    p = Particle(WIDTH * 0.5 + x, HEIGHT * 0.5 + y)
-    if i == 0:
-        p.fixed = True
-    particles.append(p)
+        
 
 def find_particle(pos):
     for i in range(len(particles)):
@@ -119,13 +104,38 @@ def find_particle(pos):
         if (dx*dx + dy*dy) < 400:
             particles[i].selected = True
             break
+            
+delta_t = 0.1
+NUM_ITER = 10
+mouse = False
+
+# create particles
+NUM_X = 10
+NUM_Y = 10
+particles = []
+for j in range(NUM_Y):
+    for i in range(NUM_X):
+        x = 100 + i * 20.0
+        y = j * 20.0
+        p = Particle(x, y)
+        particles.append(p)
+
+particles[0].fixed = True
+particles[NUM_X-1].fixed = True
 
 constraints = []
-for i in range(3):
-    index0 = i
-    index1 = (i + 1) % 3
-    c = Constraint(index0, index1)
-    constraints.append(c)
+for j in range(NUM_Y):
+    for i in range(NUM_X):
+        if i < (NUM_X - 1):
+            index0 = i + j * NUM_X
+            index1 = (i + 1) + j * NUM_X
+            c = Constraint(index0, index1)
+            constraints.append(c)
+        if j < (NUM_Y - 1):
+            index0 = i + j * NUM_X
+            index1 = i + (j + 1) * NUM_X
+            c = Constraint(index0, index1)
+            constraints.append(c)
 
 while True:
     screen.fill(BLACK)
@@ -133,14 +143,16 @@ while True:
     # particles update
     for i in range(len(particles)):
         particles[i].update(delta_t)
+
     # constraints update
-    for n in range(NUM_ITER):
-        for i in range(len(constraints)):
-            constraints[i].update()
+    for i in range(NUM_ITER):
+        for ii in range(len(constraints)):
+            constraints[ii].update()
 
     # particles draw
     for i in range(len(particles)):
         particles[i].draw(screen, 3)
+
     # constraints draw
     for i in range(len(constraints)):
         constraints[i].draw(screen, 1)
@@ -157,6 +169,6 @@ while True:
     if mouse:
         pos = pygame.mouse.get_pos()
         find_particle(pos)
-
+        
     pygame.display.update()
     fpsClock.tick(FPS)
